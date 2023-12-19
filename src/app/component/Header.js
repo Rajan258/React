8f1fe14ab2b574164c378cb21d5e1ec8f0c8fa18
rajan258 <rajan.rajak258@gmail.com>
2023-12-19 12:38:03 +0530

    second commit

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
   return (
     <>
     <div className='h-10 flex bg-black justify-between px-10 items-center text-white'>
-    <div className='flex gap-5 py-3'>
-        <Link href="About">Two way Binding</Link>
+    <div className='flex gap-5 py-3'>  
+        <Link href="About"> Two way Binding </Link>
         
         <Link href="Blogs">Routing</Link>
         <Link href="toastify">Tostify</Link>
